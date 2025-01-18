af1066a2f9c9b3699d298afe0afbeafec295d963
Dipesh <mavinash423@gmail.com>
2025-01-18 18:34:12 +0530

    shadcn

---
+++ public/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <Button>Hii</Button>
+    </div>
+  );
+}
+
+export default Login;
