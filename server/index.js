d109a849548e7f3a167fced1f44e69ef9e26bc0d
Dipesh <mavinash423@gmail.com>
2025-01-18 18:24:26 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 5000;
+
+mongoose
+  .connect(
+    "mongodb+srv://krishnaagrawal2304:P5yUtEkCqZPry8Gn@cluster0.exmph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch(() => {
+    console.error("Db Connection Error");
+  });
 
 app.get("/", (req, res) => {
   res.send("Vimarsha server running...");
